db76cd92b3b9c3df8c774bd2c51a3330da37533d
andanylo <andanylo@gmail.com>
2023-01-23 18:51:27 -0500

    Removed storyboard and added lesson model

--- LessonsAppDanylo/Model/Lesson.swift
+++ LessonsAppDanylo/Model/Lesson.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+
+///Lesson stucture with name, description, thumbnail, and video_url
+struct Lesson: Codable{
+    var name: String
+    var descritption: String
+    var thumbnail: String
+    var viedo_url: String
+}
