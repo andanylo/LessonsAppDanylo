12d21ddf3b5c7f7aae721faa181f3fa55d0749e9
andanylo <andanylo@gmail.com>
2023-01-24 16:23:10 -0500

    Added title and description labels

--- LessonsAppDanylo/ViewControllers/LessonDetailController.swift
+++ LessonsAppDanylo/ViewControllers/LessonDetailController.swift
@@ -12,16 +12,44 @@ class LessonDetailController: UIViewController{
     
     var lessonDetailViewModel: LessonDetailViewModel!
     
+    
+    ///- Returns: Video view which holds video player
     lazy var videoView: UIView = {
-       let view = UIView()
+        let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
         view.backgroundColor = .clear
         return view
     }()
-
+    
+    
+    ///- Returns: A title label which displays title of the lesson
+    lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 30, weight: .medium)
+        label.lineBreakMode = .byWordWrapping
+        label.numberOfLines = 0
+        label.textAlignment = .left
+        label.textColor = .white
+        return label
+    }()
+    
+    
+    ///- Returns: Description label which displays description of the lesson
+    lazy var descriptionLabel: UILabel = {
+       let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 18, weight: .light)
+        label.lineBreakMode = .byWordWrapping
+        label.numberOfLines = 0
+        label.textColor = .white
+        label.textAlignment = .left
+        return label
+    }()
+    
     var player: AVPlayer?
     var playerController: AVPlayerViewController?
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -38,6 +66,28 @@ class LessonDetailController: UIViewController{
         
         
         buildPlayer()
+        
+        self.view.addSubview(titleLabel)
+      
+        titleLabel.topAnchor.constraint(equalTo: self.videoView.bottomAnchor, constant: 15).isActive = true
+        titleLabel.leadingAnchor.constraint(equalTo: self.videoView.leadingAnchor, constant: 15).isActive = true
+        titleLabel.trailingAnchor.constraint(equalTo: self.videoView.trailingAnchor, constant: -15).isActive = true
+        
+        titleLabel.text = lessonDetailViewModel.lesson.name
+        titleLabel.sizeToFit()
+        
+        
+        self.view.addSubview(descriptionLabel)
+        
+        descriptionLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 15).isActive = true
+        descriptionLabel.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor).isActive = true
+        descriptionLabel.trailingAnchor.constraint(equalTo: titleLabel.trailingAnchor).isActive = true
+        
+        descriptionLabel.text = lessonDetailViewModel.lesson.description
+        descriptionLabel.sizeToFit()
+        
+        
+        
     }
     
     
